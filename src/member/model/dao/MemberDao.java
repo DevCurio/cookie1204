795efb44642553a6307a0076763752dc9ca623f1
jjh9251 <space5192@naver.com>
2021-02-27 16:57:44 +0900

    MemberDao, MemberService 생성

--- src/member/model/dao/MemberDao.java
+++ src/member/model/dao/MemberDao.java
@@ -1,8 +1,16 @@
 package member.model.dao;
 
+import static common.JDBCTemplate.close;
+
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Properties;
+
+import member.model.vo.Member;
 
 public class MemberDao {
 	
@@ -18,5 +26,43 @@ public class MemberDao {
 			e.printStackTrace();
 		}
 
+	}
+	//회원가입 정보 입력 메소드
+	public int insertMember(Connection conn, Member member) {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String query = prop.getProperty("insertMember"); 
+		
+		try {
+			//미완성쿼리문을 가지고 객체생성.
+			pstmt = conn.prepareStatement(query);
+			//쿼리문미완성
+			pstmt.setString(1, member.getMemberId());
+			pstmt.setString(2, member.getMemberPw());
+			pstmt.setString(3, member.getName());
+			pstmt.setString(4, member.getSocietyNum1());
+			pstmt.setString(5, member.getSocietyNum2());
+			pstmt.setString(6, member.getEmail());
+			pstmt.setString(7, member.getEmailGet());
+			pstmt.setString(8, member.getMobileNum());
+			pstmt.setString(9, member.getPhoneNum());
+			pstmt.setString(10, member.getZipCode());
+			pstmt.setString(11, member.getMemberAddr());
+			pstmt.setString(12, member.getMemberAddrDetail());
+			pstmt.setString(13, member.getIsAdmin());
+			pstmt.setDate(14, member.getEnrollDate());
+			
+			
+			//쿼리문실행 : 완성된 쿼리를 가지고 있는 pstmt실행(파라미터 없음)
+			//DML은 executeUpdate()
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
+		
+		return result;
 	}
 }
---
+++ src/member/model/service/MemberService.java
@@ -0,0 +1,30 @@
+package member.model.service;
+
+import static common.JDBCTemplate.close;
+import static common.JDBCTemplate.commit;
+import static common.JDBCTemplate.getConnection;
+import static common.JDBCTemplate.rollback;
+
+import java.sql.Connection;
+
+import member.model.dao.MemberDao;
+import member.model.vo.Member;
+
+public class MemberService {
+
+	public static final int ADMIN_MEMBER_ROLE = 0; //관리자
+	public static final int USER_MEMBER_ROLE = 1; //일반 회원
+	
+	private MemberDao memberDao = new MemberDao();
+	
+	public int insertMember(Member member) {
+		Connection conn = getConnection();
+		int result = memberDao.insertMember(conn, member);
+		if(result>0)
+			commit(conn);
+		else 
+			rollback(conn);
+		close(conn);
+		return result;
+	}
+}
